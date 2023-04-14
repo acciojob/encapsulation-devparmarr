738b07f74a97a2c5231dd5a11d10b60f36fcf35a
devparmarr <devparmarr123@gmail.com>
2023-04-15 03:58:03 +0530

    assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly("Dev");
+       // Get the value of name using getName method
+       String name = obj.getName();
+       System.out.println(name); 
+       
+       // Set the value of name using setName method
+       obj.setName("Mary");
+       name = obj.getName();
+       obj.printName(); 
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,23 @@
 package com.driver;
 
 public class RWOnly {
+    private String name;
+
+    public RWOnly(String name) {
+        this.name = name;
+    }
+
+     // Getter method for name
+     public String getName() {
+        return name;
+    }
     
+    // Setter method for name
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public void printName() {
+        System.out.println(name);
+    }
 }
